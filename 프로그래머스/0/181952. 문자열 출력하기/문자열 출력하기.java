4ed9857ca32c42aae5fc5d1e749176a2e6ad6de0
hangarame <82455219+hangarame@users.noreply.github.com>
2025-07-03 19:37:35 +0900

    [level 0] Title: 문자열 출력하기, Time: 208.95 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        try {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            
+            System.out.println(a);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
