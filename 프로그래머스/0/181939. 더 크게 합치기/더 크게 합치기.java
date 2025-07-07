2146c72963daccaea41a2590bc82eeb5f3e3c3dc
hangarame <82455219+hangarame@users.noreply.github.com>
2025-07-07 14:12:32 +0900

    [level 0] Title: 더 크게 합치기, Time: 15.63 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int num1 = Integer.parseInt(a + "" + b);
+        int num2 = Integer.parseInt(b + "" + a);
+        
+        if(num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+        
+        
+    }
+}
